97e8dc65825cc96a2bfc1f8195c7597ddfdefd17
Jia Chen <github@jiachen.app>
2024-11-08 22:47:52 +0800

    Initial Commit

---
+++ social cat-alog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  social cat-alog
+//
+//  Created by Jia Chen Yee on 11/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ social cat-alog/social_cat_alogApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_cat_alogApp.swift
+//  social cat-alog
+//
+//  Created by Jia Chen Yee on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct social_cat_alogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
